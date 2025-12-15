033280e3e06ef0a15956dc72f6e5b11c4e0179a5
Khrt-nisa <khoirotunnisaa1@gmail.com>
2025-12-15 14:41:29 +0700

    Add files via upload

--- ListBullet.php
+++ ListBullet.php
@@ -4,48 +4,39 @@
  * @link    https://github.com/dompdf/dompdf
  * @license http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
  */
-namespace Dompdf\FrameReflower;
+namespace Dompdf\Positioner;
 
-use Dompdf\FrameDecorator\Block as BlockFrameDecorator;
+use Dompdf\FrameDecorator\AbstractFrameDecorator;
 use Dompdf\FrameDecorator\ListBullet as ListBulletFrameDecorator;
 
 /**
- * Reflows list bullets
+ * Positions list bullets
  *
  * @package dompdf
  */
-class ListBullet extends AbstractFrameReflower
+class ListBullet extends AbstractPositioner
 {
-
     /**
-     * ListBullet constructor.
      * @param ListBulletFrameDecorator $frame
      */
-    function __construct(ListBulletFrameDecorator $frame)
+    function position(AbstractFrameDecorator $frame): void
     {
-        parent::__construct($frame);
-    }
+        // List markers are positioned to the left of the border edge of their
+        // parent element (FIXME: right for RTL)
+        $parent = $frame->get_parent();
+        $style = $parent->get_style();
+        $cbw = $parent->get_containing_block("w");
+        $margin_left = (float) $style->length_in_pt($style->margin_left, $cbw);
+        $border_edge = $parent->get_position("x") + $margin_left;
 
-    /**
-     * @param BlockFrameDecorator|null $block
-     */
-    function reflow(BlockFrameDecorator $block = null)
-    {
-        if ($block === null) {
-            return;
-        }
+        // This includes the marker indentation
+        $x = $border_edge - $frame->get_margin_width();
 
-        /** @var ListBulletFrameDecorator */
-        $frame = $this->_frame;
-        $style = $frame->get_style();
+        // The marker is later vertically aligned with the corresponding line
+        // box and its vertical position is fine-tuned in the renderer
+        $p = $frame->find_block_parent();
+        $y = $p->get_current_line_box()->y;
 
-        $style->set_used("width", $frame->get_width());
-        $frame->position();
-
-        if ($style->list_style_position === "inside") {
-            $block->add_frame_to_line($frame);
-        } else {
-            $block->add_dangling_marker($frame);
-        }
+        $frame->set_position($x, $y);
     }
 }
--- TableRow.php
+++ TableRow.php
@@ -4,79 +4,31 @@
  * @link    https://github.com/dompdf/dompdf
  * @license http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
  */
-namespace Dompdf\FrameReflower;
+namespace Dompdf\Positioner;
 
-use Dompdf\FrameDecorator\Block as BlockFrameDecorator;
-use Dompdf\FrameDecorator\Table as TableFrameDecorator;
-use Dompdf\FrameDecorator\TableRow as TableRowFrameDecorator;
-use Dompdf\Exception;
+use Dompdf\FrameDecorator\AbstractFrameDecorator;
 
 /**
- * Reflows table rows
+ * Positions table rows
  *
  * @package dompdf
  */
-class TableRow extends AbstractFrameReflower
+class TableRow extends AbstractPositioner
 {
-    /**
-     * TableRow constructor.
-     * @param TableRowFrameDecorator $frame
-     */
-    function __construct(TableRowFrameDecorator $frame)
-    {
-        parent::__construct($frame);
-    }
 
     /**
-     * @param BlockFrameDecorator|null $block
+     * @param AbstractFrameDecorator $frame
      */
-    function reflow(BlockFrameDecorator $block = null)
+    function position(AbstractFrameDecorator $frame): void
     {
-        /** @var TableRowFrameDecorator */
-        $frame = $this->_frame;
+        $cb = $frame->get_containing_block();
+        $p = $frame->get_prev_sibling();
 
-        // Check if a page break is forced
-        $page = $frame->get_root();
-        $page->check_forced_page_break($frame);
-
-        // Bail if the page is full
-        if ($page->is_full()) {
-            return;
+        if ($p) {
+            $y = $p->get_position("y") + $p->get_margin_height();
+        } else {
+            $y = $cb["y"];
         }
-
-        // Counters and generated content
-        $this->_set_content();
-
-        $this->_frame->position();
-        $style = $this->_frame->get_style();
-        $cb = $this->_frame->get_containing_block();
-
-        foreach ($this->_frame->get_children() as $child) {
-            $child->set_containing_block($cb);
-            $child->reflow();
-
-            if ($page->is_full()) {
-                break;
-            }
-        }
-
-        if ($page->is_full()) {
-            return;
-        }
-
-        $table = TableFrameDecorator::find_parent_table($this->_frame);
-        $cellmap = $table->get_cellmap();
-        $style->set_used("width", $cellmap->get_frame_width($this->_frame));
-        $style->set_used("height", $cellmap->get_frame_height($this->_frame));
-
-        $this->_frame->set_position($cellmap->get_frame_position($this->_frame));
-    }
-
-    /**
-     * @throws Exception
-     */
-    public function get_min_max_width(): array
-    {
-        throw new Exception("Min/max width is undefined for table rows");
+        $frame->set_position($cb["x"], $y);
     }
 }
